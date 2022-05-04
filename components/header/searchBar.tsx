047e6ba135d5123b54fe26ae9b003c33ea5903a0
Moomi98 <kymkjh2002@gmail.com>
2022-05-04 13:23:43 +0900

    검색창 clear 기능 적용

--- components/header/searchBar.tsx
+++ components/header/searchBar.tsx
@@ -1,24 +1,41 @@
 import styles from "components/header/searchBar.module.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { MdClear } from "react-icons/md";
 
 const SearchBar = () => {
   const [search, setSearch] = useState("");
+  const searchRef = useRef<HTMLInputElement>(null);
 
   const handleTyping = (e: any) => {
     setSearch(e.target.value);
+  };
+
+  const clearText = () => {
+    if (searchRef.current) {
+      searchRef.current.value = "";
+      setSearch("");
+    }
   };
 
   return (
     <div className={styles.layout}>
       <FaSearch className={styles.searchIcon} size="20" />
       <input
+        ref={searchRef}
         onChange={handleTyping}
         className={styles.search}
         placeholder="Search items, collections, and accounts"
       />
-      {search.length > 0 ? <MdClear size="20" color="gray" /> : null}
+      {search.length > 0 ? (
+        <MdClear
+          className={styles.clear}
+          onClick={clearText}
+          size="20"
+          color="gray"
+          style={{ cursor: "pointer" }}
+        />
+      ) : null}
     </div>
   );
 };
