6c911641c9794ca7615e2457b3e20da45d18ccb1
Moomi98 <kymkjh2002@gmail.com>
2022-05-16 20:58:28 +0900

    메인 페이지 반응형 폰트로 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
     <Layout>
       <Head>
         <title>Make Yout NFT</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Main />
     </Layout>
