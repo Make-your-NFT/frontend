b7c8f6226c0f2d8e294bb753d82c5ab983973e92
Moomi98 <kymkjh2002@gmail.com>
2022-05-18 23:53:09 +0900

    지갑 모달 레이아웃 구현 중

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -10,42 +10,22 @@ import SideBar from "@components/sidebar/sideBar";
 import { useWeb3React } from "@web3-react/core";
 import { injected } from "@utils/connectors";
 import MyWallet from "./myWallet";
-import { unwatchFile } from "fs";
 
 const Header = () => {
+  const { chainId, account, active, activate, deactivate } = useWeb3React();
   const [sideBar, setSideBar] = useState(false);
   const [showWallet, setShowWallet] = useState(false);
-  const { chainId, account, active, activate, deactivate } = useWeb3React();
 
   const handleSideBar = () => {
     sideBar ? setSideBar(false) : setSideBar(true);
   };
 
-  const handleWallet = () => {
-    showWallet ? setShowWallet(false) : setShowWallet(true);
+  const openWallet = () => {
+    setShowWallet(true);
   };
 
-  useEffect(() => {
-    if (active) {
-      console.log(chainId);
-      console.log(account);
-    }
-  }, [active]);
-  const handleConnect = () => {
-    if (active) {
-      console.log(account);
-      deactivate();
-      return;
-    }
-    const errorText: string =
-      "/No Ethereum provider was found on window.ethereum/";
-    activate(injected, (error) => {
-      if (errorText === error.message) {
-        window.open("https://metamask.io/download.html");
-      }
-
-      console.log(error);
-    });
+  const closeWallet = () => {
+    setShowWallet(false);
   };
 
   return (
@@ -72,7 +52,7 @@ const Header = () => {
             </Link>
           </li>
           <li className={styles.navItem}>
-            <div onClick={handleConnect} onMouseEnter={handleWallet}>
+            <div onMouseEnter={openWallet}>
               {active ? (
                 <RiAccountCircleLine
                   className={styles.navIcon}
@@ -88,7 +68,11 @@ const Header = () => {
               )}
             </div>
           </li>
-          {showWallet ? <MyWallet /> : null}
+          {showWallet ? (
+            <div onMouseLeave={closeWallet}>
+              <MyWallet />
+            </div>
+          ) : null}
         </div>
       </ul>
       <div className={styles.menu} onClick={handleSideBar}>
--- components/header/myWallet.tsx
+++ components/header/myWallet.tsx
@@ -1,11 +1,37 @@
 import styles from "components/header/myWallet.module.css";
 import { BsWallet } from "react-icons/bs";
+import { useWeb3React } from "@web3-react/core";
+import { injected } from "@utils/connectors";
 const MyWallet = () => {
+  const { chainId, account, active, activate, deactivate } = useWeb3React();
+
+  const handleConnect = () => {
+    if (active) {
+      console.log(account);
+      deactivate();
+      return;
+    }
+    const errorText: string =
+      "/No Ethereum provider was found on window.ethereum/";
+    activate(injected, (error) => {
+      if (errorText === error.message) {
+        window.open("https://metamask.io/download.html");
+      }
+
+      console.log(error);
+    });
+  };
+
   return (
-    <div className={styles.layout}>
-      <BsWallet size={80} />
-      <span className={styles.h1}>지갑을 연동해주세요</span>
-      <button className={styles.connectButton}>연동하기</button>
+    <div style={{ position: "relative" }}>
+      <div className={styles.background} />
+      <div className={styles.layout}>
+        <BsWallet size={80} />
+        <span className={styles.h1}>지갑을 연동해주세요</span>
+        <button className={styles.connectButton} onClick={handleConnect}>
+          연동하기
+        </button>
+      </div>
     </div>
   );
 };
