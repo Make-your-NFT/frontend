466d88e31cdb0b28bc1820d5a710dcfe9e578156
Moomi98 <kymkjh2002@gmail.com>
2022-05-19 13:50:09 +0900

    searchBar click animation 구현

--- components/header/searchBar.tsx
+++ components/header/searchBar.tsx
@@ -1,12 +1,22 @@
 import styles from "components/header/searchBar.module.css";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect} from "react";
 import { FaSearch } from "react-icons/fa";
 import { MdClear } from "react-icons/md";
 
 const SearchBar = () => {
   const [search, setSearch] = useState("");
+  const [click, setClick] = useState(false);
   const searchRef = useRef<HTMLInputElement>(null);
 
+  const handleClick = (e: any) => {
+    if(e.target.localName !== "input"){
+      setClick(false);
+    }
+    else{
+          click ? setClick(false) : setClick(true);
+
+    }
+  }
   const handleTyping = (e: any) => {
     setSearch(e.target.value);
   };
@@ -18,8 +28,17 @@ const SearchBar = () => {
     }
   };
 
+  useEffect(() =>{
+    window.addEventListener("click", handleClick);
+
+    return () => {
+      window.removeEventListener("click", handleClick);
+
+    }
+  }, [])
+
   return (
-    <div className={styles.layout}>
+    <div className={`${!click ? styles.layout : styles.layoutActive}`} onClick={handleClick}>
       <FaSearch className={styles.searchIcon} size="20" />
       <input
         ref={searchRef}
