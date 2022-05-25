8ed8a75123c5b9af507776a321f03e8328bafd3f
Moomi98 <kymkjh2002@gmail.com>
2022-05-25 22:35:47 +0900

    font 적용

--- components/signup/index.tsx
+++ components/signup/index.tsx
@@ -1,16 +1,58 @@
 import DaumAddress from "@components/daumPostCode/daumAddress";
 import Layout from "@components/layout";
 import styles from "components/signup/index.module.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
 const Index = () => {
+  const passwordRegExp = /^(?=.*[a-zA-Z])((?=.*\d)(?=.*\W)).{8,16}$/;
+  const emailRegExp =
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
   const [emailAuth, setEmailAuth] = useState(false);
+  const [password, setPassword] = useState(false);
   const [searchAddress, setSearchAddress] = useState(false);
   const [address, setAddress] = useState("");
+  const emailRef = useRef<HTMLInputElement>(null);
+  const checkEmailRef = useRef<HTMLSpanElement>(null);
+  const checkPasswordRef = useRef<HTMLSpanElement>(null);
 
-  const handleEmailAuth = () => {};
+  const handleEmailAuth = (e: any) => {
+    e.preventDefault();
+    const email = emailRef.current!.value;
+    const result = emailRegExp.test(email);
+
+    if (!result) {
+      checkEmailRef.current!.style.color = "red";
+      checkEmailRef.current!.innerHTML =
+        "유효한 이메일 형식으로 입력해 주세요.";
+    } else {
+      checkEmailRef.current!.innerHTML = "";
+    }
+  };
   const handleSearchAddress = (e: any) => {
     e.preventDefault();
     setSearchAddress(true);
+  };
+
+  const checkPassword = (e: any) => {
+    if (e.target.value.length === 0) {
+      setPassword(false);
+
+      checkPasswordRef.current!.style.color = "#777";
+      return;
+    }
+    const result = passwordRegExp.test(e.target.value);
+
+    if (!result) {
+      checkPasswordRef.current!.style.color = "red";
+      setPassword(false);
+    } else {
+      checkPasswordRef.current!.style.color = "green";
+      setPassword(true);
+    }
+  };
+
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(e);
   };
 
   return (
@@ -21,18 +63,39 @@ const Index = () => {
           <hr className={styles.hrBold} />
           <span className={styles.subTitle}>새로운 세상을 함께 떠나봐요</span>
         </div>
-        <form style={{ width: "100%" }}>
+        <form style={{ width: "100%" }} onSubmit={onSubmit}>
           <div className={styles.inputLayout}>
             <span className={styles.inputTitle}>이메일</span>
             <div className={styles.inputWithButtonLayout}>
-              <input className={styles.inputBar} type="email" id="email" />
-              <button className={styles.button}>인증</button>
+              <input
+                className={styles.inputBar}
+                type="email"
+                id="email"
+                ref={emailRef}
+              />
+              <button className={styles.button} onClick={handleEmailAuth}>
+                인증
+              </button>
             </div>
+            <span
+              style={{ fontSize: "12px" }}
+              className={styles.subTitle}
+              ref={checkEmailRef}
+            ></span>
           </div>
           <div className={styles.inputLayout}>
             <span className={styles.inputTitle}>비밀번호</span>
-            <input className={styles.inputBar} type="password" id="password" />
-            <span style={{ fontSize: "12px" }} className={styles.subTitle}>
+            <input
+              className={styles.inputBar}
+              type="password"
+              id="password"
+              onChange={checkPassword}
+            />
+            <span
+              style={{ fontSize: "12px" }}
+              className={styles.subTitle}
+              ref={checkPasswordRef}
+            >
               비밀번호는 8~16자 사이의 영어,숫자,특수문자를 포함하여 입력해
               주세요.
             </span>
@@ -65,6 +128,7 @@ const Index = () => {
             <span className={styles.inputTitle}>상세주소</span>
             <input className={styles.inputBar} type="text" id="detailAddress" />
           </div>
+          <button className={styles.signupButton}>회원가입</button>
         </form>
         {searchAddress ? (
           <DaumAddress
@@ -72,7 +136,6 @@ const Index = () => {
             setAddress={setAddress}
           />
         ) : null}
-        <button className={styles.signupButton}>회원가입</button>
       </div>
     </Layout>
   );
