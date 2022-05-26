902f896e32b7739cc4164ce79e3d012b40175490
Moomi98 <kymkjh2002@gmail.com>
2022-05-26 11:47:22 +0900

    이메일 인증번호 입력 추가

--- components/signup/index.tsx
+++ components/signup/index.tsx
@@ -1,16 +1,19 @@
 import DaumAddress from "@components/daumPostCode/daumAddress";
 import Layout from "@components/layout";
 import styles from "components/signup/index.module.css";
+import {MdClear, MdDone} from 'react-icons/md'
 import { useRef, useState } from "react";
 const Index = () => {
   const passwordRegExp = /^(?=.*[a-zA-Z])((?=.*\d)(?=.*\W)).{8,16}$/;
   const emailRegExp =
     /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  const [sendEmailAuth, setSendEmailAuth] = useState(false);
   const [emailAuth, setEmailAuth] = useState(false);
   const [password, setPassword] = useState(false);
   const [searchAddress, setSearchAddress] = useState(false);
   const [address, setAddress] = useState("");
   const emailRef = useRef<HTMLInputElement>(null);
+  const emailAuthRef = useRef<HTMLInputElement>(null);
   const checkEmailRef = useRef<HTMLSpanElement>(null);
   const checkPasswordRef = useRef<HTMLSpanElement>(null);
 
@@ -25,6 +28,8 @@ const Index = () => {
         "유효한 이메일 형식으로 입력해 주세요.";
     } else {
       checkEmailRef.current!.innerHTML = "";
+      setSendEmailAuth(true);
+      emailAuthRef.current!.disabled = false;
     }
   };
   const handleSearchAddress = (e: any) => {
@@ -84,6 +89,24 @@ const Index = () => {
             ></span>
           </div>
           <div className={styles.inputLayout}>
+            <span className={styles.inputTitle}>인증번호</span>
+            <input
+              className={styles.inputBar}
+              type="password"
+              id="password"
+              onChange={checkPassword}
+              disabled
+              ref={emailAuthRef}
+            />
+            <span
+              style={{ fontSize: "12px" }}
+              className={styles.subTitle}
+              ref={checkPasswordRef}
+            >
+            </span>
+          </div>
+
+          <div className={styles.inputLayout}>
             <span className={styles.inputTitle}>비밀번호</span>
             <input
               className={styles.inputBar}
@@ -96,19 +119,10 @@ const Index = () => {
               className={styles.subTitle}
               ref={checkPasswordRef}
             >
-              비밀번호는 8~16자 사이의 영어,숫자,특수문자를 포함하여 입력해
+              8~16자 사이의 영어,숫자,특수문자를 포함하여 입력해
               주세요.
             </span>
           </div>
-          {/* <div className={styles.inputLayout}>
-            <span className={styles.inputTitle}>휴대전화</span>
-            <div className={styles.inputWithButtonLayout}>
-              <div className={styles.inputFormLayout}>
-                <input className={styles.inputBar} type="email" id="email" />
-              </div>
-              <button className={styles.phoneAuthButton}>인증하기</button>
-            </div>
-          </div> */}
           <div className={styles.inputLayout}>
             <span className={styles.inputTitle}>주소</span>
             <div className={styles.inputWithButtonLayout}>
