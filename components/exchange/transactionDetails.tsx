7b13272edd54e1849ef3988ce3ecdd8534bde4cd
Moomi98 <kymkjh2002@gmail.com>
2022-07-03 17:53:44 +0900

    최근 거래 내역 레이아웃 구현

--- components/exchange/transactionDetails.tsx
+++ components/exchange/transactionDetails.tsx
@@ -8,7 +8,43 @@ const TransactionDetails = () => {
         <span className={styles.title}>최근 거래 내역</span>
         <GrPowerReset size={20} color="red" />
       </div>
+      <div className={styles.theadLayout}>
+        <span className={styles.thead}>일자</span>
+        <span className={styles.thead}>게임머니</span>
+        <span className={styles.thead}>환전금액</span>
+      </div>
       <div className={styles.detailsLayout}>
+        <div className={styles.detailsBarLayout}>
+          <span className={styles.detailText}>2022.07.03</span>
+          <span className={styles.detailText}>500,000</span>
+          <span className={styles.detailText}>2,500</span>
+        </div>
+        <div className={styles.detailsBarLayout_gray}>
+          <span className={styles.detailText}>2022.07.03</span>
+          <span className={styles.detailText}>500,000</span>
+          <span className={styles.detailText}>2,500</span>
+        </div>
+        <div className={styles.detailsBarLayout}>
+          <span className={styles.detailText}>2022.07.03</span>
+          <span className={styles.detailText}>500,000</span>
+          <span className={styles.detailText}>2,500</span>
+        </div>
+        <div className={styles.detailsBarLayout_gray}>
+          <span className={styles.detailText}>2022.07.03</span>
+          <span className={styles.detailText}>500,000</span>
+          <span className={styles.detailText}>2,500</span>
+        </div>
+        <div className={styles.detailsBarLayout}>
+          <span className={styles.detailText}>2022.07.03</span>
+          <span className={styles.detailText}>500,000</span>
+          <span className={styles.detailText}>2,500</span>
+        </div>
+        <div className={styles.detailsBarLayout_gray}>
+          <span className={styles.detailText}>2022.07.03</span>
+          <span className={styles.detailText}>500,000</span>
+          <span className={styles.detailText}>2,500</span>
+        </div>
+        {/* <div className={styles.detailsBarLayout}></div>
         <div className={styles.detailsBarLayout}></div>
         <div className={styles.detailsBarLayout}></div>
         <div className={styles.detailsBarLayout}></div>
@@ -21,9 +57,7 @@ const TransactionDetails = () => {
         <div className={styles.detailsBarLayout}></div>
         <div className={styles.detailsBarLayout}></div>
         <div className={styles.detailsBarLayout}></div>
-        <div className={styles.detailsBarLayout}></div>
-        <div className={styles.detailsBarLayout}></div>
-        <div className={styles.detailsBarLayout}></div>
+        <div className={styles.detailsBarLayout}></div> */}
       </div>
     </article>
   );
