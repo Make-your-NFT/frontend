15283b6d2cb485f0dd50c64560434f741f715897
Moomi98 <kymkjh2002@gmail.com>
2022-07-15 16:51:13 +0900

    차트 날짜 생성 구현

--- components/exchange/chart.tsx
+++ components/exchange/chart.tsx
@@ -22,44 +22,60 @@ ChartJS.register(
 );
 
 const Chart = () => {
+  const getMonthList = () => {
+    const today = new Date();
+    const monthList = [];
+
+    const currentYear: number = today.getFullYear();
+    const currentMonth: string = ("0" + (today.getMonth() + 1)).slice(-2);
+    let year: number = currentYear;
+    let month: number = Number(currentMonth);
+
+    for (let i = 0; i < 6; i++) {
+      if (month > 0) {
+        monthList.push(`${month}월`);
+        month--;
+      } else {
+        year--;
+        month = 12;
+        monthList.push(`${year}-${month}`);
+      }
+    }
+
+    return monthList.reverse();
+  };
+
   const options = {
     responsive: true,
     plugins: {
       legend: {
-        position: "top" as const,
+        display: false,
+        // position: "top" as const,
       },
       title: {
-        display: true,
-        text: "Chart.js Line Chart",
+        display: false,
+        // text: "Chart.js Line Chart",
       },
     },
   };
 
-  const labels = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-  ];
+  const labels = getMonthList();
 
   const data = {
     labels,
     datasets: [
       {
-        label: "Dataset 1",
+        label: "시세",
         data: [90, 20, 50, 10, 40, 50, 30],
         borderColor: "rgb(255, 99, 132)",
         backgroundColor: "rgba(255, 99, 132, 0.5)",
       },
-      {
-        label: "Dataset 2",
-        data: [20, 20, 10, 40, 50, 30, 70],
-        borderColor: "rgb(53, 162, 235)",
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
-      },
+      // {
+      //   label: "Dataset 2",
+      //   data: [20, 20, 10, 40, 50, 30, 70],
+      //   borderColor: "rgb(53, 162, 235)",
+      //   backgroundColor: "rgba(53, 162, 235, 0.5)",
+      // },
     ],
   };
 
